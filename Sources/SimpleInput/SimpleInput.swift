ae89d88a63acecffec9deb1d0e7f04a3730f621e
Sam Deane <sam@elegantchaos.com>
2021-09-14 17:08:46 +0100

    Added configuration options for fields.

--- Sources/SimpleInput/SimpleInput.swift
+++ Sources/SimpleInput/SimpleInput.swift
@@ -8,7 +8,24 @@ import SwiftUI
 extension UIAlertController {
     convenience init(input: SimpleInput, completion: @escaping () -> ()) {
         self.init(title: input.title, message: input.message, preferredStyle: .alert)
-        addTextField { $0.placeholder = input.placeholder }
+        addTextField {
+            let settings = input.field
+            $0.placeholder = settings.placeholder
+            $0.autocapitalizationType = settings.autocapitalizationType
+            $0.autocorrectionType = settings.autocorrectionType
+            $0.spellCheckingType = settings.spellCheckingType
+            $0.smartQuotesType = settings.smartQuotesType
+            $0.smartDashesType = settings.smartDashesType
+            $0.smartInsertDeleteType = settings.smartInsertDeleteType
+            $0.keyboardType = settings.keyboardType
+            $0.keyboardAppearance = settings.keyboardAppearance
+            $0.returnKeyType = settings.returnKeyType
+            $0.enablesReturnKeyAutomatically = settings.enablesReturnKeyAutomatically
+            $0.isSecureTextEntry = settings.isSecureTextEntry
+            if let type = settings.textContentType {
+                $0.textContentType = type
+            }
+        }
 
         for button in input.buttons {
             addAction(button.action(field: self.textFields?.first, completion: completion))
@@ -51,46 +68,93 @@ struct InputVC: UIViewControllerRepresentable{
     }
 }
 
-public enum Button {
-    case normal(String,(String) -> ())
-    case destructive(String, (String) -> ())
-    case cancel(String = "Cancel", () -> () = { })
-    
-    func action(field: UITextField? = nil, completion: @escaping () -> ()) -> UIAlertAction {
-        switch self {
-            case .normal(let title, let action):
-                return UIAlertAction(title: NSLocalizedString(title, comment: "button title"), style: .default) { _ in
-                    action(field?.text ?? "")
-                    completion()
-                }
-                
-            case .destructive(let title, let action):
-                return UIAlertAction(title: NSLocalizedString(title, comment: "button title"), style: .destructive) { _ in
-                    action(field?.text ?? "")
-                    completion()
-                }
-
-            case .cancel(let title, let action):
-                return UIAlertAction(title: NSLocalizedString(title, comment: "button title"), style: .cancel) { _ in
-                    action()
-                    completion()
-                }
-        }
-
-    }
-}
 
 public struct SimpleInput {
-    public init(title: String, message: String, placeholder: String = "", buttons: [Button]) {
+    public enum Button {
+        case normal(String,(String) -> ())
+        case destructive(String, (String) -> ())
+        case cancel(String = "Cancel", () -> () = { })
+        
+        func action(field: UITextField? = nil, completion: @escaping () -> ()) -> UIAlertAction {
+            switch self {
+                case .normal(let title, let action):
+                    return UIAlertAction(title: NSLocalizedString(title, comment: "button title"), style: .default) { _ in
+                        action(field?.text ?? "")
+                        completion()
+                    }
+                    
+                case .destructive(let title, let action):
+                    return UIAlertAction(title: NSLocalizedString(title, comment: "button title"), style: .destructive) { _ in
+                        action(field?.text ?? "")
+                        completion()
+                    }
+
+                case .cancel(let title, let action):
+                    return UIAlertAction(title: NSLocalizedString(title, comment: "button title"), style: .cancel) { _ in
+                        action()
+                        completion()
+                    }
+            }
+
+        }
+    }
+
+    public struct Field {
+        let placeholder: String
+        let autocapitalizationType: UITextAutocapitalizationType
+        let autocorrectionType: UITextAutocorrectionType
+        let spellCheckingType: UITextSpellCheckingType
+        let smartQuotesType: UITextSmartQuotesType
+        let smartDashesType: UITextSmartDashesType
+        let smartInsertDeleteType: UITextSmartInsertDeleteType
+        let keyboardType: UIKeyboardType
+        let keyboardAppearance: UIKeyboardAppearance
+        let returnKeyType: UIReturnKeyType
+        let enablesReturnKeyAutomatically: Bool
+        let isSecureTextEntry: Bool
+        let textContentType: UITextContentType?
+        
+        public init(
+            placeholder: String = "",
+            autocapitalizationType: UITextAutocapitalizationType = .sentences,
+            autocorrectionType: UITextAutocorrectionType = .default,
+            spellCheckingType: UITextSpellCheckingType = .default,
+            smartQuotesType: UITextSmartQuotesType = .default,
+            smartDashesType: UITextSmartDashesType = .default,
+            smartInsertDeleteType: UITextSmartInsertDeleteType = .default,
+            keyboardType: UIKeyboardType = .default,
+            keyboardAppearance: UIKeyboardAppearance = .default,
+            returnKeyType: UIReturnKeyType = .default,
+            enablesReturnKeyAutomatically: Bool = false,
+            isSecureTextEntry: Bool = false,
+            textContentType: UITextContentType? = nil
+            ) {
+            self.placeholder = NSLocalizedString(placeholder, comment: "input placeholder")
+            self.autocapitalizationType = autocapitalizationType
+            self.autocorrectionType = autocorrectionType
+            self.spellCheckingType = spellCheckingType
+            self.smartQuotesType = smartQuotesType
+            self.smartDashesType = smartDashesType
+            self.smartInsertDeleteType = smartInsertDeleteType
+            self.keyboardType = keyboardType
+            self.keyboardAppearance = keyboardAppearance
+            self.returnKeyType = returnKeyType
+            self.enablesReturnKeyAutomatically = enablesReturnKeyAutomatically
+            self.isSecureTextEntry = isSecureTextEntry
+            self.textContentType = textContentType
+        }
+    }
+    
+    public init(title: String, message: String, field: Field, buttons: [Button]) {
         self.title = NSLocalizedString(title, comment: "input title")
         self.message = NSLocalizedString(message, comment: "input message")
-        self.placeholder = NSLocalizedString(placeholder, comment: "input placeholder")
+        self.field = field
         self.buttons = buttons
     }
     
     public let title: String
     public let message: String
-    public let placeholder: String
+    public let field: Field
     public let buttons: [Button]
 }
 
